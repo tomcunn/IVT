b96f5a0b9cddad82e0221e1b5bf240439b21bd2e
Tom <tomacunningham@gmail.com>
2019-08-27 07:24:22 -0500

    Uploading IVT  library, new CAN message

--- IVT.cpp
+++ IVT.cpp
@@ -12,7 +12,7 @@ IVT::IVT()
 //*******************************************************
 void IVT:: init()
 {
-   //Configure the pins 
+   //Set the output pins
    pinMode(MotorA_PWM_Channel ,OUTPUT);
    pinMode(MotorA_DIR_ChannelA,OUTPUT);
    pinMode(MotorA_DIR_ChannelB,OUTPUT);
@@ -21,12 +21,18 @@ void IVT:: init()
    pinMode(MotorB_DIR_ChannelA,OUTPUT);
    pinMode(MotorB_DIR_ChannelB,OUTPUT);
    
-   //Set the pin modes
+   //Set the inputs pin
    pinMode(MotorA_Encoder_Channel_A,INPUT);
    pinMode(MotorA_Encoder_Channel_B,INPUT);
-
    pinMode(MotorB_Encoder_Channel_A,INPUT);
    pinMode(MotorB_Encoder_Channel_B,INPUT);
+   pinMode(JOYSTICKX, INPUT);
+   pinMode(JOYSTICKY, INPUT);
+   pinMode(BARE_POT, INPUT);
+   pinMode(V_MOTOR_A3, INPUT);
+   pinMode(A4, INPUT);
+   pinMode(A5, INPUT);
+   
    
    delay(500);
    
@@ -93,3 +99,86 @@ void IVT:: SetMotor(int ChannelA, int ChannelB, int PWM_Channel, int pwm_value, 
       analogWrite(PWM_Channel , 0); 
    }
 }
+
+//*******************************************************
+//   ReadBatteryVoltage 
+//   
+//   Read in the battery pack voltage, return mv.
+//*******************************************************
+int IVT:: ReadBatteryVoltage()
+{
+   int BatteryVoltage_ADC;
+   int BatteryVoltage_mV;
+   
+   //Read in the voltage of the following PIN, this is 10 bit
+   BatteryVoltage_ADC =  analogRead(V_MOTOR_A3); 
+   
+   //Convert from ADC to mV, assume a 5v reference
+   //Use 512 instead of 1024 since there is a voltage divider that 
+   //divides by 2. 
+   BatteryVoltage_mV = int(long(BatteryVoltage_ADC) * 5000 / 512);
+   
+   return BatteryVoltage_mV;
+}
+
+//*********************************************************
+//   SendCANMessage
+//
+//   Send a serial message, not the CAN protocol but still teaches 
+//   the fundamentals.
+//**********************************************************
+void IVT:: SendCANMessage(int data[])
+{
+   String datastring;
+   String temp;
+   
+   datastring = "8 ";
+   
+   //Load the data and convert to hex.
+   for(int i = 0; i < 8; i++)
+   {
+      datastring += " ";
+      temp = String(data[i], HEX);
+      //Pad the string
+      if(temp.length() == 1)
+      {
+          temp = "0"+temp;
+      }
+      datastring += temp;
+
+   }
+   Serial.println(datastring);
+}
+
+//***********************************************************
+//  GetJoystickValue
+//
+//  Pass in the 10 bit ADC value from the joystick and the end points
+//  and this will return a value from -100 to 100, the joystick 
+//  can be inteverted with the last argument
+//***********************************************************
+/*signed int IVT:: GetJoystickValue(int ADC, int Low_EndPoint_ADC, int High_EndPoint_ADC, bool inverted)
+{
+   int range;
+   int midpoint;
+   signed int handle_percent;
+   
+   //Calculate the range
+   range = abs(High_EndPoint_ADC - Low_EndPoint_ADC);
+   
+   //scale 200% is the full scale range, instead of dividing by 200 use 20.
+   scalex10 = range/20;
+   
+   handle_percent = (ADC-midpoint)*scalex10/10;  
+   if(handle_percent > 100)
+   {
+	   handle_percent = 100;
+   }
+   else if(handle_percent <-100)
+   {
+	   handle_percent = -100;
+   }
+   
+   return handle_percent;
+}
+*/
--- IVT.h
+++ IVT.h
@@ -23,11 +23,20 @@
 #define  MotorB_Encoder_Channel_A  3
 #define  MotorB_Encoder_Channel_B  11
 
+//These #defines are based on the schematic names
+#define JOYSTICKX    14
+#define JOYSTICKY    15
+#define BARE_POT     16
+#define V_MOTOR_A3   17
+#define A4           18
+#define A5           19
+
 enum MOTOR_ID_ENUM
 {
    MotorA,
    MotorB
 };
+
 
 
 // Your class header here...
@@ -36,10 +45,21 @@ class IVT
     public:
        //Constructor, required, same name as class
        IVT();
+       
        //Sets up all the pins properly for the I/O needed to control the L298
        void init();
+       
        //Used by the application developer to control the motors
        void SetMotorPWM(MOTOR_ID_ENUM motor_id, int pwm_value, int direction);
+       
+       //Read Battery Voltage, returns an int in mV
+       int ReadBatteryVoltage();
+       
+       //Send a sudo CAN messages
+       void SendCANMessage(int data[]);
+       
+       //Get the motor position
+      // int GetMotorPosition(MOTOR_ID_ENUM motor_id);
 
     private:
        //Used internally to load the values for the motor.
